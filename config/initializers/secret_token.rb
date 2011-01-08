eb5f7d7e232bb2d79ddde8e3aeb8673bb6e1399a
danigb <danigb@gmail.com>
2011-01-08 16:10:44 +0100

    Rails3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recortable::Application.config.secret_token = '8ada5f5907764a93bd271fb9ffb902b13e86941ccd97080297d91e7eb69fdeec463a41a3e41d77dc9c4dcd409a098451d319e735591c726e283c00d2ed36f0b6'
